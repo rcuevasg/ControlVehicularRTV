736cb2c3ab8f5e280a327aea865f08d5dbb4bdbd
Raul Cuevas Gorocica <raul8527@gmail.com>
2013-04-11 14:13:38 -0500

    a
    a

--- admin/connection.php
+++ admin/connection.php
@@ -11,7 +11,7 @@ try
     //$dbname = "rcgdesar_ctv";
     //$user = "dante";
     $user = "root";
-    $pass = "root13";
+    $pass = "";
     //$pass = "";
     $DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
